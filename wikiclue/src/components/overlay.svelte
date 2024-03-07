4ed374c47dbcf97b4d347a40ed7f837605a7324e
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-06 23:04:56 -0700

    feat: added start game overlays

---
+++ wikiclue/src/components/overlay.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { fade } from "svelte/transition";
+
+    export let header = "";
+    export let onClose = () => {};
+</script>
+  
+<div class="overlay" on:click={() => onClose()} transition:fade>
+    <div class="popup" on:click|stopPropagation>
+        <button class="close-button" on:click={() => onClose()}>&times;</button>
+        <h2 class="popup-title">{header}</h2>
+        <slot></slot>
+    </div>
+</div>
+
+<style>
+    @import '../styles/componentStyles/overlayStyles.css';
+</style>
