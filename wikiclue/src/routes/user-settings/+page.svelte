f7e37e6ceed6009cf4a09fd33d51e256bba9a557
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-07 22:26:07 -0700

    fix: removed unecessary scroll bar on user settings page

--- wikiclue/src/routes/user-settings/+page.svelte
+++ wikiclue/src/routes/user-settings/+page.svelte
@@ -7,8 +7,8 @@
 <div class="user-settings-page">
   <form>
       <h1>User Settings</h1>
-      <button on:click={console.log("dummy button, replace")}>Change Password</button>
-      <button on:click={console.log("dummy button, replace")}>Change Username</button>
+      <button on:click={()=>console.log("dummy button, replace")}>Change Password</button>
+      <button on:click={()=>console.log("dummy button, replace")}>Change Username</button>
   </form>
 </div>
 
