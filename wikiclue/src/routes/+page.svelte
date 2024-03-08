8b3debf44475da59895ad130bc69c884cf797b53
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-08 01:48:37 -0700

    feat: added guest play mode

--- wikiclue/src/routes/+page.svelte
+++ wikiclue/src/routes/+page.svelte
@@ -1,19 +1,42 @@
 <script lang="ts">
-    let searchTerm = "";
+	import { browser } from "$app/environment";
     import { goto } from "$app/navigation";
+	import { guestPlay } from "../store/gameplay";
+
+    function play() {
+        if (browser) {
+            guestPlay.streakCount.subscribe(value => {
+                sessionStorage.setItem('streakCount', JSON.stringify(value));
+            });
+            guestPlay.timeRemaining.subscribe(value => {
+                sessionStorage.setItem('timeRemaining', JSON.stringify(value));
+            });
+
+            guestPlay.skipsRemaining.subscribe(value => {
+                sessionStorage.setItem('skipsRemaining', JSON.stringify(value));
+            });
+            // Will need to change this to some function that actually generates random words
+            sessionStorage.setItem('firstWord', JSON.stringify("First"));
+            sessionStorage.setItem('secondWord', JSON.stringify("Second"));
+            //
+            goto("/play");
+        }
+    }
 </script>
 
 <div class="landing-page">
     <h1>Welcome to WikiClue</h1>
-    <p class = "sub-header">Find a Wikipedia page that contains the following words!</p>
-    <div class="words-container">
-        <p class="search-words">Sample</p>
-        <p class="search-words">Words</p>
-    </div>
-    <input type="text" class="search-bar" placeholder="Start typing to play..." bind:value={searchTerm}/>
-    <div class="buttons-container">
-        <button on:click={()=>goto("/login")}>Login</button>
-        <button on:click={()=>goto("/signup")}>Sign Up</button>
+    <div class="content-container">
+        <button on:click={()=>play()}>Play</button>
+        <div class="divider-container">
+            <div class="divider"/>
+                <p class="divider-text">or</p>
+            <div class="divider"/>
+        </div>
+        <div class="buttons-container">
+            <button on:click={()=>goto("/login")}>Login</button>
+            <button on:click={()=>goto("/signup")}>Sign Up</button>
+        </div>
     </div>
 </div>
 
--- wikiclue/src/store/gameplay.ts
+++ wikiclue/src/store/gameplay.ts
@@ -1,5 +1,10 @@
 import { writable } from 'svelte/store';
 
+// Guest Gameplay Variables
+const guestStreakCount = writable(0);
+const guestTimeRemaining = writable(60);
+const guestSkipsRemaining = writable(3);
+
 // Levels Variables if needed
 
 // The Daily Variables if needed
@@ -8,6 +13,12 @@ import { writable } from 'svelte/store';
 const rushStreakCount = writable(0);
 const rushTimeRemaining = writable(60);
 const rushSkipsRemaining = writable(3);
+
+const guestPlay = {
+	streakCount: guestStreakCount,
+	timeRemaining: guestTimeRemaining,
+	skipsRemaining: guestSkipsRemaining
+};
 
 const levels = {
 	// Insert levels variables if needed
@@ -23,4 +34,4 @@ const rush = {
 	skipsRemaining: rushSkipsRemaining
 };
 
-export { rush, levels, theDaily };
+export { guestPlay, rush, levels, theDaily };
