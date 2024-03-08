17c89a96955718130517393ae97ee0f20f6583ca
Colin-C32 <colin.christophe@ucalgary.ca>
2024-03-08 15:39:56 -0700

    feat: add end screens

---
+++ wikiclue/src/components/levelEnd.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import Overlay from "./overlay.svelte";
+	import { writable } from "svelte/store";
+    export let levelOpen = false;
+
+    let currentLevel = 12;
+    let levelWords = ["Jockey", "Betting"];
+    let userAnswer = "Betting on horse racing";
+    const isOverlayOpen = writable(false);
+</script>
+
+
+<Overlay header="Level {currentLevel}" onClose={() => {isOverlayOpen.set(false); levelOpen = false;}}>
+    <p class="popup-text">Congratulations!</p>
+    <div class="level-answer">
+        <p class="popup-text">You completed level {currentLevel}:</p>
+        <h3 class="score">{levelWords[0]}</h3>
+        <h3 class="score">{levelWords[1]}</h3>
+    </div>
+    <div class="level-answer">
+        <p class="popup-text">With you answer:</p>
+        <h3 class="score">{userAnswer}</h3>
+    </div>
+    
+    <div class="bottom-options">
+        <button>Play Next Level</button>
+        <button>Return to Main Menu</button>
+    </div>
+</Overlay>
+
+<style>
+    @import '../styles/componentStyles/endScreenStyles.css';
+</style>
