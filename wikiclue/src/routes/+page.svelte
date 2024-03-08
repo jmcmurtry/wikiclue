73b15e7012a0b70aded1f2f3c269568988ea9e81
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-08 01:58:37 -0700

    feat: added guest play start popup

--- wikiclue/src/routes/+page.svelte
+++ wikiclue/src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import { browser } from "$app/environment";
     import { goto } from "$app/navigation";
+	import { writable } from "svelte/store";
 	import { guestPlay } from "../store/gameplay";
+    import RushIcon from '~icons/nimbus/fire'
+    import TimerIcon from '~icons/material-symbols/timer-outline'
+    import SkipIcon from '~icons/bi/skip-forward'
+	import Overlay from "../components/overlay.svelte";
+
+    const isOverlayOpen = writable(false);
 
     function play() {
         if (browser) {
@@ -27,7 +34,7 @@
 <div class="landing-page">
     <h1>Welcome to WikiClue</h1>
     <div class="content-container">
-        <button on:click={()=>play()}>Play</button>
+        <button on:click={()=>isOverlayOpen.set(true)}>Play</button>
         <div class="divider-container">
             <div class="divider"/>
                 <p class="divider-text">or</p>
@@ -38,6 +45,23 @@
             <button on:click={()=>goto("/signup")}>Sign Up</button>
         </div>
     </div>
+    {#if $isOverlayOpen}
+        <Overlay header="Play!" onClose={() => {isOverlayOpen.set(false)}}>
+            <div class="popup-row">
+                <RushIcon style="font-size: 2.5em; color: black"/>
+                <p class="popup-text">Try to achieve the longest streak you can</p>
+            </div>
+            <div class="popup-row">
+                <TimerIcon style="font-size: 2.5em; color: black"/>
+                <p class="popup-text">You are allowed 3 skips per game</p>
+            </div>
+            <div class="popup-row">
+                <SkipIcon style="font-size: 2.5em; color: black"/>
+                <p class="popup-text">You have up to 3 minutes per round</p>
+            </div>
+            <button class="popup-button" on:click={() => play()}>Play Now!</button>
+        </Overlay>   
+    {/if}
 </div>
 
 <style>
