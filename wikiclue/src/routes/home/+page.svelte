f97d6bf8c7b79ba731422572f410c522c2019501
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-03-08 20:03:23 -0700

    feat: add play levels screen

--- wikiclue/src/routes/home/+page.svelte
+++ wikiclue/src/routes/home/+page.svelte
@@ -25,126 +25,86 @@
 				sessionStorage.setItem('timeRemaining', JSON.stringify(value));
 			});
 
-			rush.skipsRemaining.subscribe((value) => {
-				sessionStorage.setItem('skipsRemaining', JSON.stringify(value));
-			});
-			// Will need to change this to some function that actually generates random words
-			sessionStorage.setItem('firstWord', JSON.stringify('First'));
-			sessionStorage.setItem('secondWord', JSON.stringify('Second'));
-			//
-			goto('/rush');
-		}
-	}
+            rush.skipsRemaining.subscribe(value => {
+                sessionStorage.setItem('skipsRemaining', JSON.stringify(value));
+            });
+            // Will need to change this to some function that actually generates random words
+            sessionStorage.setItem('firstWord', JSON.stringify("First"));
+            sessionStorage.setItem('secondWord', JSON.stringify("Second"));
+            //
+            goto("/rush");
+        }
+    }
+
+    function playLevels() {
+        if (browser) {
+            // Will need to change this to some function that actually generates random words
+            sessionStorage.setItem('firstWord', JSON.stringify("King"));
+            sessionStorage.setItem('secondWord', JSON.stringify("Harra"));
+            //
+            goto("/levels");
+        }
+    }
 </script>
 
 <HeaderBar />
 <div class="home-page">
-	<h1>Welcome to WikiClue</h1>
-	<div class="games-container">
-		<div class="game-container">
-			<h2>Levels</h2>
-			<LevelsIcon style="font-size: 2.5em; color: black; padding-top: 5px" />
-			<p class="game-description">Think you can complete 30 levels of increasing difficulty?</p>
-			<button
-				on:click={() => {
-					isOverlayOpen.set(true);
-					levelsOpen = true;
-				}}>Play Levels</button
-			>
-		</div>
-		<div class="game-container">
-			<h2>The Daily</h2>
-			<DailyIcon style="font-size: 2.5em; color: black; padding-top: 5px" />
-			<p class="game-description">How fast can you solve the daily problem?</p>
-			<button
-				on:click={() => {
-					isOverlayOpen.set(true);
-					dailyOpen = true;
-				}}>Play The Daily</button
-			>
-		</div>
-		<div class="game-container">
-			<h2>Rush</h2>
-			<RushIcon style="font-size: 2.5em; color: black; padding-top: 5px" />
-			<p class="game-description">How big of a streak can you get?</p>
-			<button
-				on:click={() => {
-					isOverlayOpen.set(true);
-					rushOpen = true;
-				}}>Play Rush</button
-			>
-		</div>
-	</div>
-	<div class="buttons-container">
-		<button on:click={() => goto('/add-friends')}>Add Friends</button>
-		<button>View Leaderboards</button>
-	</div>
-	{#if $isOverlayOpen && levelsOpen}
-		<Overlay
-			header="Levels"
-			onClose={() => {
-				isOverlayOpen.set(false);
-				levelsOpen = false;
-			}}
-		>
-			<p class="popup-description">
-				In this game mode you will have unlimited time to try and complete 30 levels of increasing
-				difficulty! Are you ready for the challenge?
-			</p>
-			<LevelsIcon style="font-size: 5.0rem; color: black; margin: 5%" />
-			<button class="popup-button">Play Now!</button>
-		</Overlay>
-	{/if}
-	{#if $isOverlayOpen && dailyOpen}
-		<Overlay
-			header="The Daily"
-			onClose={() => {
-				isOverlayOpen.set(false);
-				dailyOpen = false;
-			}}
-		>
-			<p class="popup-description">
-				Solve the daily problem as fast as possible! Come back everyday to build your streak!
-			</p>
-			<DailyIcon style="font-size: 6rem; color: black; margin: 10%" />
-			<button
-				class="popup-button"
-				on:click={() => {
-					goto('/daily');
-				}}>Play Now!</button
-			>
-		</Overlay>
-	{/if}
-	{#if $isOverlayOpen && rushOpen}
-		<Overlay
-			header="Rush"
-			onClose={() => {
-				isOverlayOpen.set(false);
-				rushOpen = false;
-			}}
-		>
-			<div class="popup-row">
-				<div class="icon-container">
-					<RushIcon style="font-size: 2.5em; color: black" />
-				</div>
-				<p class="popup-text">Try to achieve the longest streak you can</p>
-			</div>
-			<div class="popup-row">
-				<div class="icon-container">
-					<TimerIcon style="font-size: 2.5em; color: black" />
-				</div>
-				<p class="popup-text">You are allowed 3 skips per game</p>
-			</div>
-			<div class="popup-row">
-				<div class="icon-container">
-					<SkipIcon style="font-size: 2.5em; color: black" />
-				</div>
-
-				<p class="popup-text">You have up to 3 minutes per round</p>
-			</div>
-			<button class="popup-button" on:click={() => playRush()}>Play Now!</button>
-		</Overlay>
-	{/if}
+    <h1>Welcome to WikiClue</h1>
+    <div class="games-container">
+        <div class="game-container">
+            <h2>Levels</h2>
+            <LevelsIcon style="font-size: 2.5em; color: black; padding-top: 5px"/>
+            <p class="game-description">Think you can complete 30 levels of increasing difficulty?</p>
+            <button on:click={() => {isOverlayOpen.set(true); levelsOpen = true;}}>Play Levels</button>
+        </div>
+        <div class="game-container">
+            <h2>The Daily</h2>
+            <DailyIcon style="font-size: 2.5em; color: black; padding-top: 5px"/>
+            <p class="game-description">How fast can you solve the daily problem?</p>
+            <button on:click={() => {isOverlayOpen.set(true); dailyOpen = true;}}>Play The Daily</button>
+        </div>
+        <div class="game-container">
+            <h2>Rush</h2>
+            <RushIcon style="font-size: 2.5em; color: black; padding-top: 5px"/>
+            <p class="game-description">How big of a streak can you get?</p>
+            <button on:click={() => {isOverlayOpen.set(true); rushOpen = true;}}>Play Rush</button>
+        </div>
+    </div>
+    <div class="buttons-container">
+        <button on:click={()=>goto("/add-friends")}>Add Friends</button>
+        <button>View Leaderboards</button>
+    </div>
+    {#if $isOverlayOpen && levelsOpen}
+        <Overlay header="Levels" onClose={() => {isOverlayOpen.set(false); levelsOpen = false;}}>
+            <p class="popup-description">In this game mode you will have unlimited time to try and complete 30 levels of increasing difficulty! Are you ready for the challenge?</p>
+            <LevelsIcon style="font-size: 4rem; color: black; margin-bottom: 10%"/>
+            <button on:click={() => playLevels()}>Play Now!</button>
+        </Overlay>   
+    {/if}
+    {#if $isOverlayOpen && dailyOpen}
+        <Overlay header="The Daily" onClose={() => {isOverlayOpen.set(false); dailyOpen = false;}}>
+            <p class="popup-description">Solve the daily problem as fast as possible! Come back everyday to build your streak!</p>
+            <DailyIcon style="font-size: 4rem; color: black; margin-bottom: 10%"/>
+            <button>Play Now!</button>
+        </Overlay>   
+    {/if}
+    {#if $isOverlayOpen && rushOpen}
+        <Overlay header="Rush" onClose={() => {isOverlayOpen.set(false); rushOpen = false;}}>
+            <div class="popup-row">
+                <RushIcon style="font-size: 2.5em; color: black"/>
+                <p class="popup-text">Try to achieve the longest streak you can</p>
+            </div>
+            <div class="popup-row">
+                <TimerIcon style="font-size: 2.5em; color: black"/>
+                <p class="popup-text">You are allowed 3 skips per game</p>
+            </div>
+            <div class="popup-row">
+                <SkipIcon style="font-size: 2.5em; color: black"/>
+                <p class="popup-text">You have up to 3 minutes per round</p>
+            </div>
+            <button on:click={() => playRush()}>Play Now!</button>
+        </Overlay>   
+    {/if}
 </div>
 
 <style>
