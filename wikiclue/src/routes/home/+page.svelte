612c8412a86d2eb6fe8c46d7e7f1b69f3055c367
Colin-C32 <colin.christophe@ucalgary.ca>
2024-03-09 13:30:14 -0700

    fix: homepage styles

--- wikiclue/src/routes/home/+page.svelte
+++ wikiclue/src/routes/home/+page.svelte
@@ -68,31 +68,38 @@
         <Overlay header="Levels" onClose={() => {isOverlayOpen.set(false); levelsOpen = false;}}>
             <p class="popup-description">In this game mode you will have unlimited time to try and complete 30 levels of increasing difficulty! Are you ready for the challenge?</p>
             <LevelsIcon style="font-size: 6rem; color: black; margin: 10%"/>
-            <button>Play Now!</button>
+            <button class="popup-button">Play Now!</button>
         </Overlay>   
     {/if}
     {#if $isOverlayOpen && dailyOpen}
         <Overlay header="The Daily" onClose={() => {isOverlayOpen.set(false); dailyOpen = false;}}>
             <p class="popup-description">Solve the daily problem as fast as possible! Come back everyday to build your streak!</p>
             <DailyIcon style="font-size: 6rem; color: black; margin: 10%"/>
-            <button>Play Now!</button>
+            <button class="popup-button">Play Now!</button>
         </Overlay>   
     {/if}
     {#if $isOverlayOpen && rushOpen}
         <Overlay header="Rush" onClose={() => {isOverlayOpen.set(false); rushOpen = false;}}>
             <div class="popup-row">
-                <RushIcon style="font-size: 3.5em; color: black"/>
+                <div class="icon-container">
+                    <RushIcon style="font-size: 2.5em; color: black"/>
+                </div>
                 <p class="popup-text">Try to achieve the longest streak you can</p>
             </div>
             <div class="popup-row">
-                <TimerIcon style="font-size: 3.5em; color: black"/>
+                <div class="icon-container">
+                    <TimerIcon style="font-size: 2.5em; color: black"/>
+                </div>
                 <p class="popup-text">You are allowed 3 skips per game</p>
             </div>
             <div class="popup-row">
-                <SkipIcon style="font-size: 3.5em; color: black"/>
+                <div class="icon-container">
+                    <SkipIcon style="font-size: 2.5em; color: black"/>
+                </div>
+                
                 <p class="popup-text">You have up to 3 minutes per round</p>
             </div>
-            <button on:click={() => playRush()}>Play Now!</button>
+            <button class="popup-button" on:click={() => playRush()}>Play Now!</button>
         </Overlay>   
     {/if}
 </div>
