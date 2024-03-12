3421e158f3f6646a4fbb09243a4a0f0e1a32dca0
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-03-12 17:35:55 -0600

    fix: start on verification

--- wikiclue/src/routes/change-password/+page.svelte
+++ wikiclue/src/routes/change-password/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import HeaderBar from "../../components/headerBar.svelte";
+  import Overlay from "../../components/overlay.svelte";
   import { authHandlers } from "../../store/store";
+  import { writable } from "svelte/store";
 
   let currentPassword = "";
   let newPassword = "";
   let confirmNewPassword = "";
   let errorMessage = "";
+  let recentLoginEmail = "";
+  let recentLoginPassword = "";
+  const needRecentLogin = writable(false);
 
   async function changePasswordClicked(){
     try {
@@ -18,13 +23,19 @@
           return;
       }
       await authHandlers.changePassword(newPassword);
-      // See if there is a change password function in firebase
       currentPassword = "";
       newPassword = "";
       confirmNewPassword = "";
       errorMessage = "";
     } catch (error: any) {
-      errorMessage = error.message;
+      if (error.message === "Firebase: Error (auth/weak-password)"){
+        errorMessage = "Password Reset Failed - Weak Password";
+      } else if (error.message === "Firebase: Error (auth/requires-recent-login)."){
+        needRecentLogin.set(true);
+        // errorMessage = "Invalid email, try again.";
+      } else {
+          errorMessage = error.message;
+      }
     }
   }
 </script>
@@ -33,18 +44,27 @@
 
 <div class="change-password-page">
   <h1>Change Password</h1>
-    <form>
-        {#if errorMessage}
-        <p class="error-message">{errorMessage}</p>
-        {/if}
-        <p class ="input-label">Current Password</p>
-        <input type="password" class="change-password-input" placeholder="Enter your current password..." bind:value={currentPassword}/>
-        <p class ="input-label">New Password</p>
-        <input type="password" class="change-password-input" placeholder="Enter your new password..." bind:value={newPassword}/>
-        <p class ="input-label">Confirm New Password</p>
-        <input type="password" class="change-password-input" placeholder="Enter your new password again..." bind:value={confirmNewPassword}/>
-        <button on:click={() => changePasswordClicked()}>Change Password</button>
-    </form>
+  <form>
+      {#if errorMessage}
+      <p class="error-message">{errorMessage}</p>
+      {/if}
+      <p class ="input-label">Current Password</p>
+      <input type="password" class="change-password-input" placeholder="Enter your current password..." bind:value={currentPassword}/>
+      <p class ="input-label">New Password</p>
+      <input type="password" class="change-password-input" placeholder="Enter your new password..." bind:value={newPassword}/>
+      <p class ="input-label">Confirm New Password</p>
+      <input type="password" class="change-password-input" placeholder="Enter your new password again..." bind:value={confirmNewPassword}/>
+      <button on:click={() => changePasswordClicked()}>Change Password</button>
+  </form>
+  {#if $needRecentLogin}
+    <Overlay header="Verify yourself by logging in" onClose={() => {needRecentLogin.set(false);}}>
+        <p class ="input-label">Email</p>
+        <input type="text" class="login-input" placeholder="Enter your email..." bind:value={recentLoginEmail}/>
+        <p class ="input-label">Password</p>
+        <input type="password" class="login-input" placeholder="Enter your password..." bind:value={recentLoginPassword}/>
+        <button class="popup-button">Kill me :)</button>
+    </Overlay>
+  {/if}
 </div>
 
 <style>
