9f170a413f5d1bed0ce44bea7eb45900914e642f
Kenny Jeon <kenny.jeon25@gmail.com>
2024-03-12 16:24:16 -0600

    added leaderboard

---
+++ wikiclue/src/routes/leaderboard/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import HeaderBar from "../../components/headerBar.svelte";
+    import Overlay from "../../components/overlay.svelte";
+    import { goto } from '$app/navigation';
+    import { browser } from '$app/environment';
+
+
+</script>
+
+<HeaderBar />
+<div class="leaderboards">
+    <h1>Leaderboards</h1>
+    <div class="leaderboard-container">
+        <div class="leader-container">
+            <h2>Levels</h2>
+            <p class="leader-description">Highest Level your friends have reached</p>
+        </div>
+        <div class="leader-container">
+            <h2>The Daily</h2>
+            <p class="leader-description">Amount of guess it took your friends to solve The Daily</p>
+        </div>
+        <div class="leader-container">
+            <h2>Rush</h2>
+            <p class="leader-description">Longest Rush streak your friends have achieved</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    @import '../../styles/leaderboardStyles.css';
+</style>
