3b34142ac077e9c1f7b7df5fe85cffe56d927a72
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-13 15:42:25 -0600

    fix: removed console logs in server.ts

--- wikiclue/src/routes/+server.ts
+++ wikiclue/src/routes/+server.ts
@@ -5,7 +5,6 @@ const filePath = 'static/assets/words.txt';
 let allWords: string[];
 
 async function loadAllWords(filePath: string) {
-	console.log('Loading words from file in function');
 	try {
 		const data = await file.promises.readFile(filePath, 'utf8');
 		allWords = data.split('\n').filter((word: string) => word.length > 4);
@@ -19,7 +18,6 @@ loadAllWords(filePath);
 export async function GET() {
 	if (!allWords) {
 		await loadAllWords(filePath);
-		console.log('Loading words from file');
 	}
 	const randomIndex1 = Math.floor(Math.random() * allWords.length);
 	const randomIndex2 = Math.floor(Math.random() * allWords.length);
