50c1271660944af937900d652b4b5bdee5e3ceea
Kenny Jeon <kenny.jeon25@gmail.com>
2024-03-16 22:54:15 -0600

    added popup when friend is added

--- wikiclue/src/routes/friend-requests/+page.svelte
+++ wikiclue/src/routes/friend-requests/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import HeaderBar from "../../components/headerBar.svelte";
+    import Overlay from '../../components/overlay.svelte';
+    import { writable } from 'svelte/store';
+
+    const friendAddOverlayVis = writable(false);
 
     function addFriend(){
-        //addFriend
+        friendAddOverlayVis.set(true);
     }
     function declineRequest(){
         //declineFriendRequest
@@ -16,13 +20,19 @@
         <div class="friends-container">
             <h3>These users want to be your friend!</h3>
             <div class ="users-container">
-                <p class="friend-name">LitLover911<button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button></p>
-                <p class="friend-name">Chris_Morroco <button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button></p>
-                <p class="friend-name">Bonnie <button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button></p>
-                <p class="friend-name">Clyde <button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button> </p>
+                <p class="friend-name">LitLover911<button class="deny-button" on:click={() => declineRequest()}>&#10006;</button><button class="accept-button" on:click={() => addFriend()}>&#10004;</button></p>
+                <p class="friend-name">Chris_Morroco <button class="deny-button" on:click={() => declineRequest()}>&#10006;</button><button class="accept-button" on:click={() => addFriend()}>&#10004;</button></p>
+                <p class="friend-name">Bonnie <button class="deny-button" on:click={() => declineRequest()}>&#10006;</button><button class="accept-button" on:click={() => addFriend()}>&#10004;</button></p>
+                <p class="friend-name">Clyde <button class="deny-button" on:click={() => declineRequest()}>&#10006;</button><button class="accept-button" on:click={() => addFriend()}>&#10004;</button> </p>
             </div>
         </div>
     </div>
+
+    {#if $friendAddOverlayVis}
+        <Overlay header="New Friend!" onClose={() => {friendAddOverlayVis.set(false)}}>
+            <p class="popup-text">Friend Added!</p>
+        </Overlay>   
+    {/if}
 </div>
 
 <style>
