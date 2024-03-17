42115366910c5621799f4784253dd17c99bfe57f
Kenny Jeon <kenny.jeon25@gmail.com>
2024-03-16 22:31:23 -0600

    added base add friends page

---
+++ wikiclue/src/routes/friend-requests/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import HeaderBar from "../../components/headerBar.svelte";
+
+    function addFriend(){
+        //addFriend
+    }
+    function declineRequest(){
+        //declineFriendRequest
+    }
+</script>
+
+<HeaderBar />
+<div class="friend-requests">
+    <h1>Friend Requests</h1>
+    <div class="friendboard-container">
+        <div class="friends-container">
+            <h3>These users want to be your friend!</h3>
+            <div class ="users-container">
+                <p class="friend-name">LitLover911<button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button></p>
+                <p class="friend-name">Chris_Morroco <button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button></p>
+                <p class="friend-name">Bonnie <button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button></p>
+                <p class="friend-name">Clyde <button class="deny-button" on:click={() => onClose()}>&#10006;</button><button class="accept-button" on:click={() => onClose()}>&#10004;</button> </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    @import '../../styles/friendRequestStyles.css';
+</style>
