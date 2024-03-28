1f842f29a8f3df630ccf538865fd8b88ffbfa72f
Kenny Jeon <kenny.jeon25@gmail.com>
2024-03-28 11:17:26 -0600

    added admin header, also overhauled admin home page to look proper

---
+++ wikiclue/src/routes/admin/admin-settings/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import HeaderBar from "../../../components/headerBar.svelte";
+  import { goto } from '$app/navigation';
+</script>
+
+<HeaderBar />
+
+<div class="user-settings-page">
+  <form>
+      <h1>Settings</h1>
+      <p class ="label">Account:</p>
+      <hr/>
+      <button on:click={()=>setTimeout(() => goto('/change-password'), 0)}>Change Password</button>
+      <button on:click={()=>setTimeout(() => goto('/change-username'), 0)}>Change Username</button>
+  </form>
+</div>
+
+<style>
+  @import '../../../styles/userSettingsPageStyles.css';
+</style>
