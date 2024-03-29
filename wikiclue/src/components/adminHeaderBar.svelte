d63505ed67f3f0a77cdd13578e2aeb84bcdd9b08
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-03-29 17:23:11 -0600

    fix: fixed admin header bar z index

--- wikiclue/src/components/adminHeaderBar.svelte
+++ wikiclue/src/components/adminHeaderBar.svelte
@@ -1,63 +1,63 @@
-<script lang ="ts">
-    import logo from '$lib/images/logo.png';
-    import ProfileIcon from '~icons/iconoir/profile-circle';
-    import LogoutIcon from "~icons/material-symbols/logout-sharp"
-    import BurgerMenuIcon from "~icons/material-symbols/menu"
+<script lang="ts">
+	import logo from '$lib/images/logo.png';
+	import ProfileIcon from '~icons/iconoir/profile-circle';
+	import LogoutIcon from '~icons/material-symbols/logout-sharp';
+	import BurgerMenuIcon from '~icons/material-symbols/menu';
 	import { authHandlers } from '../store/store';
 	import { onMount } from 'svelte';
 
-    let showBurgerMenu = false;
-    let burgerMenuOpen = false;
+	let showBurgerMenu = false;
+	let burgerMenuOpen = false;
 
-    onMount(() => {
-        const mediaQuery = window.matchMedia('(max-width: 768px)');
-        showBurgerMenu = mediaQuery.matches;
-        const handler = (event) => {
-            showBurgerMenu = event.matches;
-        };
-        mediaQuery.addListener(handler);
-        return () => {
-            mediaQuery.removeListener(handler);
-        };
-    });
+	onMount(() => {
+		const mediaQuery = window.matchMedia('(max-width: 768px)');
+		showBurgerMenu = mediaQuery.matches;
+		const handler = (event) => {
+			showBurgerMenu = event.matches;
+		};
+		mediaQuery.addListener(handler);
+		return () => {
+			mediaQuery.removeListener(handler);
+		};
+	});
 
-    function toggleBurgerMenu() {
-        burgerMenuOpen = !burgerMenuOpen;
-    }
+	function toggleBurgerMenu() {
+		burgerMenuOpen = !burgerMenuOpen;
+	}
 
-    async function logout() {
-        await authHandlers.logout();
-    }
+	async function logout() {
+		await authHandlers.logout();
+	}
 </script>
 
 <nav class="header-bar">
-    <div id="left-align">
-        <a href="/admin">
-            <img src={ logo } alt="WikiClue Logo" />
-        </a>
-    </div>
-    {#if !showBurgerMenu}
-        <div class="links-container">
-            <a href="/admin/admin-settings">
-                <ProfileIcon style="font-size: 1.5rem; color: white; padding-top: 5px"/>
-            </a>
-            <button class="logout-button" on:click={() => logout()}>
-                <LogoutIcon style="font-size: 1.5rem; color: white; padding-top: 5px"/>
-            </button>
-        </div>
-    {:else}
-        <button class="burger-menu" on:click={() => toggleBurgerMenu()}>
-            <BurgerMenuIcon style="font-size: 1.5rem; color: white; padding-top: 2px"/>
-        </button>
-        {#if burgerMenuOpen}
-            <div class="burger-menu-items">
-                <a href="/admin/admin-settings">Gamemaster Settings</a>
-                <button on:click={() => logout()}>Logout</button>
-            </div>
-        {/if}
-    {/if}
+	<div id="left-align">
+		<a href="/admin">
+			<img src={logo} alt="WikiClue Logo" />
+		</a>
+	</div>
+	{#if !showBurgerMenu}
+		<div class="links-container">
+			<a href="/admin/admin-settings">
+				<ProfileIcon style="font-size: 1.5rem; color: white; padding-top: 5px" />
+			</a>
+			<button class="logout-button" on:click={() => logout()}>
+				<LogoutIcon style="font-size: 1.5rem; color: white; padding-top: 5px" />
+			</button>
+		</div>
+	{:else}
+		<button class="burger-menu" on:click={() => toggleBurgerMenu()}>
+			<BurgerMenuIcon style="font-size: 1.5rem; color: white; padding-top: 2px" />
+		</button>
+		{#if burgerMenuOpen}
+			<div class="burger-menu-items">
+				<a href="/admin/admin-settings">Gamemaster Settings</a>
+				<button on:click={() => logout()}>Logout</button>
+			</div>
+		{/if}
+	{/if}
 </nav>
 
 <style>
-    @import '../styles/componentStyles/headerBarStyles.css';
+	@import '../styles/componentStyles/headerBarStyles.css';
 </style>
--- wikiclue/src/components/headerBar.svelte
+++ wikiclue/src/components/headerBar.svelte
@@ -1,73 +1,73 @@
-<script lang ="ts">
-    import logo from '$lib/images/logo.png';
-    import TrophyIcon from '~icons/octicon/trophy-24';
-    import AddFriendIcon from '~icons/fluent-mdl2/add-friend';
-    import ProfileIcon from '~icons/iconoir/profile-circle';
-    import LogoutIcon from "~icons/material-symbols/logout-sharp"
-    import BurgerMenuIcon from "~icons/material-symbols/menu"
+<script lang="ts">
+	import logo from '$lib/images/logo.png';
+	import TrophyIcon from '~icons/octicon/trophy-24';
+	import AddFriendIcon from '~icons/fluent-mdl2/add-friend';
+	import ProfileIcon from '~icons/iconoir/profile-circle';
+	import LogoutIcon from '~icons/material-symbols/logout-sharp';
+	import BurgerMenuIcon from '~icons/material-symbols/menu';
 	import { authHandlers } from '../store/store';
 	import { onMount } from 'svelte';
 
-    let showBurgerMenu = false;
-    let burgerMenuOpen = false;
+	let showBurgerMenu = false;
+	let burgerMenuOpen = false;
 
-    onMount(() => {
-        const mediaQuery = window.matchMedia('(max-width: 768px)');
-        showBurgerMenu = mediaQuery.matches;
-        const handler = (event) => {
-            showBurgerMenu = event.matches;
-        };
-        mediaQuery.addListener(handler);
-        return () => {
-            mediaQuery.removeListener(handler);
-        };
-    });
+	onMount(() => {
+		const mediaQuery = window.matchMedia('(max-width: 768px)');
+		showBurgerMenu = mediaQuery.matches;
+		const handler = (event) => {
+			showBurgerMenu = event.matches;
+		};
+		mediaQuery.addListener(handler);
+		return () => {
+			mediaQuery.removeListener(handler);
+		};
+	});
 
-    function toggleBurgerMenu() {
-        burgerMenuOpen = !burgerMenuOpen;
-    }
+	function toggleBurgerMenu() {
+		burgerMenuOpen = !burgerMenuOpen;
+	}
 
-    async function logout() {
-        await authHandlers.logout();
-    }
+	async function logout() {
+		await authHandlers.logout();
+	}
 </script>
 
 <nav class="header-bar">
-    <div id="left-align">
-        <a href="/home">
-            <img src={ logo } alt="WikiClue Logo" />
-        </a>
-    </div>
-    {#if !showBurgerMenu}
-        <div class="links-container">
-            <a href="/leaderboard">
-                <TrophyIcon style="font-size: 1.5rem; color: white; padding-top: 5px"/>
-            </a>
-            <a href="/add-friends">
-                <AddFriendIcon style="font-size: 1.5rem; color: white; padding-top: 5px"/>
-            </a>
-            <a href="/user-settings">
-                <ProfileIcon style="font-size: 1.5rem; color: white; padding-top: 5px"/>
-            </a>
-            <button class="logout-button" on:click={() => logout()}>
-                <LogoutIcon style="font-size: 1.5rem; color: white; padding-top: 5px"/>
-            </button>
-        </div>
-    {:else}
-        <button class="burger-menu" on:click={() => toggleBurgerMenu()}>
-            <BurgerMenuIcon style="font-size: 1.5rem; color: white; padding-top: 2px"/>
-        </button>
-        {#if burgerMenuOpen}
-            <div class="burger-menu-items">
-                <a href="/leaderboard">Leaderboard</a>
-                <a href="/add-friends">Add Friends</a>
-                <a href="/user-settings">User Settings</a>
-                <button on:click={() => logout()}>Logout</button>
-            </div>
-        {/if}
-    {/if}
+	<div id="left-align">
+		<a href="/home">
+			<img src={logo} alt="WikiClue Logo" />
+		</a>
+	</div>
+	{#if !showBurgerMenu}
+		<div class="links-container">
+			<a href="/leaderboard">
+				<TrophyIcon style="font-size: 1.5rem; color: white; padding-top: 5px" />
+			</a>
+			<a href="/add-friends">
+				<AddFriendIcon style="font-size: 1.5rem; color: white; padding-top: 5px" />
+			</a>
+			<a href="/user-settings">
+				<ProfileIcon style="font-size: 1.5rem; color: white; padding-top: 5px" />
+			</a>
+			<button class="logout-button" on:click={() => logout()}>
+				<LogoutIcon style="font-size: 1.5rem; color: white; padding-top: 5px" />
+			</button>
+		</div>
+	{:else}
+		<button class="burger-menu" on:click={() => toggleBurgerMenu()}>
+			<BurgerMenuIcon style="font-size: 1.5rem; color: white; padding-top: 2px" />
+		</button>
+		{#if burgerMenuOpen}
+			<div class="burger-menu-items">
+				<a href="/leaderboard">Leaderboard</a>
+				<a href="/add-friends">Add Friends</a>
+				<a href="/user-settings">User Settings</a>
+				<button on:click={() => logout()}>Logout</button>
+			</div>
+		{/if}
+	{/if}
 </nav>
 
 <style>
-    @import '../styles/componentStyles/headerBarStyles.css';
+	@import '../styles/componentStyles/headerBarStyles.css';
 </style>
