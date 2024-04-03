dc3c411477b907e4138f906ca8ab123147967acb
John McMurtry <77893842+jmcmurtry@users.noreply.github.com>
2024-04-03 17:54:07 -0600

    feat: added route protection for admin pages and a spinner

--- wikiclue/src/routes/+layout.svelte
+++ wikiclue/src/routes/+layout.svelte
@@ -1,52 +1,70 @@
 <script lang="ts">
-	import { auth, db } from '../firebase/firebase';
+	import { auth } from '../firebase/firebase';
     import '../global.css'
     import { onMount } from 'svelte'
 	import { authStore } from '../store/store';
-    import { isAdmin } from '../store/admin';
     import { allowOnMount } from '../store/mount';
+    import Spinner from '../components/spinner.svelte';
 
     const nonAuthRoutes = ['/login', '/signup', '/', '/play', '/forgot-password'];
-
-    
+    const adminRoutes = ['/admin', '/admin/rush', '/admin/admin-settings', '/admin/change-password', '/admin/change-username', '/admin/daily', '/admin/levels'];
+    let isPageReady = false;
+    let isRedirected = false;
 
     onMount(() => {
-      let unsubscribe: any;
-      const unsubscribeStore = allowOnMount.subscribe((value: any) => {
-          if (!value) {
-              if (unsubscribe) unsubscribe();
-              return;
-          }
-          unsubscribe = auth.onAuthStateChanged(async (user) => {
-              authStore.update(() => ({
-                  user: user || null
-              }));
+        let unsubscribe: any;
+        const unsubscribeStore = allowOnMount.subscribe((value: any) => {
 
-              const currentPath = window.location.pathname;
-
-              if (!user && !nonAuthRoutes.includes(currentPath)) {
-                window.location.href = '/login';
+            if (!value) {
+                if (unsubscribe) unsubscribe();
                 return;
-              }
+            }
 
-              if (user && nonAuthRoutes.includes(currentPath)) {
-                isAdmin.subscribe((value) => {
-			        if(!value) {
+            unsubscribe = auth.onAuthStateChanged(async (user) => {
+                authStore.update(() => ({
+                    user: user || null
+                }));
+
+                const currentPath = window.location.pathname;
+
+                if (!user && !nonAuthRoutes.includes(currentPath)) {
+                    window.location.href = '/login';
+                    return;
+                }
+
+                if (user && adminRoutes.some(route => currentPath.startsWith(route))) {
+                    const isAdminUser = await isAdminUserCheck(user);
+                    if (!isAdminUser && !isRedirected) {
                         window.location.href = '/home';
-                    } else {
-                        window.location.href = '/admin';
+                        isRedirected = true;
+                        return;
                     }
-		        });
-                return;
-              }
-          });
-      });
+                }
 
-      return () => {
-          unsubscribeStore();
-          if (unsubscribe) unsubscribe();
-      };
-  });
+                if (user && nonAuthRoutes.includes(currentPath)) {
+                    console.log("aquie");
+                    window.location.href = '/home';
+                    return;
+                }
+
+                isPageReady = true;
+            });
+        });
+
+        return () => {
+            unsubscribeStore();
+            if (unsubscribe) unsubscribe();
+        };
+    });
+
+    async function isAdminUserCheck(user: any) {
+        const userClaims = await user.getIdTokenResult();
+        return userClaims.claims.user_id === import.meta.env.VITE_FIREBASE_ADMIN_ID;
+    }
 </script>
 
-<slot/>
+<Spinner isLoading={!isPageReady} />
+
+{#if isPageReady}
+    <slot/>
+{/if}
