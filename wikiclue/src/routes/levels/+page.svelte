f2e7cf8c20700f40bcfa0f2501c1d6f737d72193
Harry <77893070+harrisonMondragon@users.noreply.github.com>
2024-04-03 23:32:34 -0600

    fix: load all level data onMount in /levels

--- wikiclue/src/routes/levels/+page.svelte
+++ wikiclue/src/routes/levels/+page.svelte
@@ -1,92 +1,91 @@
 <script lang="ts">
-  import HeaderBar from "../../components/headerBar.svelte";
-  import GameHeader from "../../components/gameHeader.svelte";
-  import LockIcon from '~icons/carbon/locked';
-  import { goto } from '$app/navigation';
-  import { writable } from "svelte/store";
-  import { auth } from "../../firebase/firebase";
-  import { authHandlers } from "../../store/store";
-  import { onMount } from "svelte";
+    import HeaderBar from "../../components/headerBar.svelte";
+    import GameHeader from "../../components/gameHeader.svelte";
+    import LockIcon from '~icons/carbon/locked';
+    import { goto } from '$app/navigation';
+    import { writable } from "svelte/store";
+    import { auth } from "../../firebase/firebase";
+    import { authHandlers } from "../../store/store";
+    import { onMount } from "svelte";
 
-  let selectedDifficulty = 'easy';
-  let levelData: any[];
-  let userData: any;
-  let userLevelsData: any;
-  let maxLevel: number;
+    let selectedDifficulty = 'easy';
+    let difficultyInt = 0;
+    let levelData: any[] = [undefined, undefined, undefined];
+    let userData: any;
+    let userLevelsData: any;
 
-  onMount (async () => {
-    await new Promise<void>((resolve) => {
-        auth.onAuthStateChanged(async (user: any) => {
-            userData = user;
-            if(userData){
-                userLevelsData = await authHandlers.getUserCurrentLevelsData(userData.uid);
-            }
-            resolve();
+    onMount (async () => {
+        await new Promise<void>((resolve) => {
+            auth.onAuthStateChanged(async (user: any) => {
+                userData = user;
+                if(userData){
+                    userLevelsData = await authHandlers.getUserCurrentLevelsData(userData.uid);
+                }
+                resolve();
+            });
         });
+
+        // Load all level data
+        levelData[0] = await authHandlers.getLevels('easy');
+        levelData[1] = await authHandlers.getLevels('medium');
+        levelData[2] = await authHandlers.getLevels('hard');
     });
 
-    // Default to easy
-    maxLevel = userLevelsData[0];
-    levelData = await authHandlers.getLevels('easy');
-  });
+    async function handleOptionChange(event: { target: any; }) {
+        selectedDifficulty = event.target.value;
+        if (selectedDifficulty === "easy"){
+            difficultyInt = 0;
+        }
+        else if (selectedDifficulty === "medium"){
+            difficultyInt = 1;
+        }
+        else if (selectedDifficulty === "hard"){
+            difficultyInt = 2;
+        }
+    }
 
-  async function handleOptionChange(event: { target: any; }) {
-      selectedDifficulty = event.target.value;
-      levelData = await authHandlers.getLevels(selectedDifficulty);
-      if (selectedDifficulty === "easy"){
-          maxLevel = userLevelsData[0];
-      }
-      else if (selectedDifficulty === "medium"){
-          maxLevel = userLevelsData[1];
-      }
-      else if (selectedDifficulty === "hard"){
-          maxLevel = userLevelsData[2];
-      }
-      console.log(maxLevel);
-  }
-
-  function handleLevelClick(level: number) {
-      goto(`/levels/${selectedDifficulty}-${level+1}`);
-  }
+    function handleLevelClick(level: number) {
+        goto(`/levels/${selectedDifficulty}-${level+1}`);
+    }
 
 </script>
 
 
 <HeaderBar />
 <div class="select-level-page">
-  <GameHeader header="Pick a level" arrow={true} backLink="/home"/>
+    <GameHeader header="Pick a level" arrow={true} backLink="/home"/>
 
-    <div class="change-difficulty">
-        <h3>Difficulty:</h3>
-        <div class="select">
-            <select class="dropdown" on:change={handleOptionChange}>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
-            </select>
-            <span class="focus"></span>
+        <div class="change-difficulty">
+            <h3>Difficulty:</h3>
+            <div class="select">
+                <select class="dropdown" on:change={handleOptionChange}>
+                    <option value="easy">Easy</option>
+                    <option value="medium">Medium</option>
+                    <option value="hard">Hard</option>
+                </select>
+                <span class="focus"></span>
+            </div>
         </div>
-    </div>
 
-  <div class="level-options-container">
-      {#if levelData}
-          {#each Object.keys(levelData) as level, index}
-              {#if index <= maxLevel}
-                  <button class="level" on:click={() => handleLevelClick(index)}>
-                    {index+1}
-                  </button>
-              {:else}
-                  <button class="level" disabled={true}>
-                    <LockIcon style="font-size: 1.5rem; color: black" />
-                  </button>
-              {/if}
-          {/each}
-      {:else}
-          <p>Loading...</p>
-      {/if}
-  </div>
+    <div class="level-options-container">
+        {#if levelData[difficultyInt]}
+            {#each Object.keys(levelData[difficultyInt]) as level, index}
+                {#if index <= userLevelsData[difficultyInt]}
+                    <button class="level" on:click={() => handleLevelClick(index)}>
+                        {index+1}
+                    </button>
+                {:else}
+                    <button class="level" disabled={true}>
+                        <LockIcon style="font-size: 1.5rem; color: black" />
+                    </button>
+                {/if}
+            {/each}
+        {:else}
+            <p>Loading...</p>
+        {/if}
+    </div>
 </div>
 
 <style>
-  @import '../../styles/selectLevelPageStyles.css';
+    @import '../../styles/selectLevelPageStyles.css';
 </style>
